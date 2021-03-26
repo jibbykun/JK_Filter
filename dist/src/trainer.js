98aaae1a4f5f125c9b3c987f906525a3f5fe20d3
keyanij <keyanij@coventry.ac.uk>
2021-03-26 16:23:57 +0000

    Added error handling

--- dist/src/trainer.js
+++ dist/src/trainer.js
@@ -3,8 +3,26 @@ let filterData = {
     data: []
 };
 
+function isEmpty(object) {
+    for(let prop in object) {
+        if(object.hasOwnProperty(prop))
+            return false;
+    }
+    return true;
+}
+
+function updateStatus(status){
+    document.getElementById('status-jkfilter').innerText = status;
+
+    setTimeout(function(){ document.getElementById('status-jkfilter').innerText = "Status: "; }, 3000);
+}
+
 function exportData(filename){
     chrome.storage.sync.get(filterId, function(chromeData) {
+        if (isEmpty(chromeData[filterId])) {
+            updateStatus('Status: Error. No filtered data found.');
+            return;
+        }
         let file = new Blob([ JSON.stringify(chromeData.filterData.data) ], { type: 'text/plain' });
         var a = document.createElement('a');
         a.href = URL.createObjectURL(file);
@@ -14,18 +32,7 @@ function exportData(filename){
     chrome.storage.sync.remove(filterId, function() {
         console.log("Data cleared.");
     });
-}
-
-function isEmpty(object) {
-    for(var prop in object) {
-        if(object.hasOwnProperty(prop))
-            return false;
-    }
-    return true;
-}
-
-function updateStatus(status){
-    document.getElementById('status-jkfilter').innerText = status;
+    updateStatus('Status: Data exported.');
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -37,33 +44,37 @@ document.addEventListener('DOMContentLoaded', function() {
     trainer.addEventListener('click', function() {
         let inputData = document.getElementById('data-jkfilter').value;
         let filter = document.getElementById('filter-jkfilter').value === "filter";
-        let data = {
-            data: []
-        };
+        if(!inputData || !filter){
+            updateStatus('Status: Error. Please fill in the fields correctly.');
+        } else {
+            let data = {
+                data: []
+            };
 
-        chrome.storage.sync.get(filterId, function(chromeData) {
-            if (isEmpty(chromeData[filterId])) {
+            chrome.storage.sync.get(filterId, function(chromeData) {
+                if (isEmpty(chromeData[filterId])) {
+                    let object = {};
+                    object[filterId] = data;
+                    chrome.storage.sync.set(object, function(d){
+                        console.log("Initialised.");
+                    });
+                } else {
+                    data = chromeData[filterId];
+                    console.log(chromeData);
+                }
+                data.data.push(
+                    {comment: inputData, toxic: filter}
+                );
+
                 let object = {};
                 object[filterId] = data;
                 chrome.storage.sync.set(object, function(d){
-                    console.log("Initialised.");
+                    console.log("Data saved.");
                 });
-            } else {
-                data = chromeData[filterId];
-                console.log(chromeData);
-            }
-            data.data.push(
-                {comment: inputData, toxic: filter}
-            );
-
-            let object = {};
-            object[filterId] = data;
-            chrome.storage.sync.set(object, function(d){
-                console.log("Data saved.");
             });
-        });
 
-        updateStatus('Status: Data added.');
+            updateStatus('Status: Data added.');
+        }
     });
 
     clear.addEventListener('click', function() {
@@ -75,12 +86,10 @@ document.addEventListener('DOMContentLoaded', function() {
 
     exportTrain.addEventListener('click', function() {
         exportData('trainingData');
-        updateStatus('Status: Data exported.');
     });
 
     exportTest.addEventListener('click', function() {
         exportData('testingData');
-        updateStatus('Status: Data exported.');
     });
 });
 
