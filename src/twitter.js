3bab11c2b122c6fe03986df9cece9be4656a0772
keyanij <keyanij@coventry.ac.uk>
2021-03-26 16:23:09 +0000

    Updated JSON GitHub link

--- src/twitter.js
+++ src/twitter.js
@@ -4,22 +4,26 @@ const useLoader = require('@tensorflow-models/universal-sentence-encoder');
 
 async function predictToxic(){
     try {
-        const model = await tf.loadLayersModel('https://github.coventry.ac.uk/raw/keyanij/JK_tfjs_model/master/model.json');
+        const model = await tf.loadLayersModel('https://raw.githubusercontent.com/jibbykun/JK_tfjs_model/main/model.json');
         console.log('Starting load');
         const use = await useLoader.load();
         console.log('Loaded Universal Sentence Encoder');
-        document.querySelectorAll('div.css-901oao.r-18jsvk2.r-1qd0xha.r-a023e6.r-16dba41.r-rjixqe.r-bcqeeo.r-bnwqim.r-qvutc0').forEach(async function(node) {
-            console.log('Starting encoding');
-            const embedding = await use.embed(node.innerText.toLowerCase());
-            console.log('Encoding complete.');
-            const toxic = tf.tensor([0.9]);
-            const prediction = model.predict(embedding);
-            prediction.print();
-            const result = prediction.greaterEqual(toxic);
-            const arrResult = Array.from(result.dataSync());
-            if (arrResult[0] === 1)
-                node.innerText = "";
-        });
+        function predictTweet() {
+            document.querySelectorAll('div.css-901oao.r-18jsvk2.r-1qd0xha.r-a023e6.r-16dba41.r-rjixqe.r-bcqeeo.r-bnwqim.r-qvutc0').forEach(async function (node) {
+                console.log('Starting encoding');
+                const embedding = await use.embed(node.innerText.toLowerCase());
+                console.log('Encoding complete.');
+                const toxic = tf.tensor([0.9]);
+                const prediction = model.predict(embedding);
+                prediction.print();
+                const result = prediction.greaterEqual(toxic);
+                const arrResult = Array.from(result.dataSync());
+                if (arrResult[0] === 1)
+                    node.innerText = "";
+            });
+            setTimeout(function(){predictTweet();},30000);
+        }
+        predictTweet();
     } catch (err){
         console.log(err);
     }
